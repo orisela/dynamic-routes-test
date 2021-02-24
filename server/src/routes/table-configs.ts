cb42355d7eac984c392eed9ae3fc2c626c418e01
orisela <orisela3@gmail.com>
2021-02-24 12:36:10 +0200

    Create findOne endpoint

--- server/src/routes/table-configs.ts
+++ server/src/routes/table-configs.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 const router = express.Router();
+
 import tableConfigs from '../../assets/table-configs.json';
 
 router.get('/', function(req, res) {
   res.json(tableConfigs);
+});
+
+router.get('/:service', function(req, res) {
+  const { service } = req.params;
+  res.json(tableConfigs.find((conf) => conf.service === service))
 });
 
 export default router;
